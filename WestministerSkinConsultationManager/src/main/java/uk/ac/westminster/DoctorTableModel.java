0fc2b90c530d6ea685999b6b893a5b9418d21253
Hasit Methmal Jayasekara <62075022+hasithmeth@users.noreply.github.com>
2023-07-17 00:03:45 +0530

    Update DoctorTableModel.java

--- WestministerSkinConsultationManager/src/main/java/uk/ac/westminster/DoctorTableModel.java
+++ WestministerSkinConsultationManager/src/main/java/uk/ac/westminster/DoctorTableModel.java
@@ -1,7 +1,6 @@
 package uk.ac.westminster;
 
 import javax.swing.table.AbstractTableModel;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DoctorTableModel extends AbstractTableModel {
